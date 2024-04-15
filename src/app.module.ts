6bc18340a8e7c9b31a1d014b07416777699c546a
Victor Ozoterio <victorozoterio@gmail.com>
2024-04-14 23:01:06 -0300

    ENVs configuradas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123',
-      database: 'lista',
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'mariadb',
+        host: ConfigService.get('DB_HOST', 'localhost'),
+        port: Number(ConfigService.get('DB_PORT', 3306)),
+        username: ConfigService.get('DB_USERNAME', 'root'),
+        password: ConfigService.get('DB_PASSWORD', '123'),
+        database: ConfigService.get('DB_DATABASE', 'lista'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
-})
+  })
 export class AppModule {}
